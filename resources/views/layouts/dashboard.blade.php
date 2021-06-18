1e6e959542bb09802898aea2444a838e12cf4fdd
Roberto Attanasio <r.attanasio90@gmail.com>
2021-06-18 09:42:42 +0200

    piccole correzioni HTML

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -11,6 +11,10 @@
     
         {{-- FONT AWESOME --}}
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+        {{-- CDN VUE.JS --}}
+        <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.20.0/axios.min.js"></script>
     
         {{-- STYLES CSS --}}
         <link rel="stylesheet" href="{{asset('css/styles.css')}}">
