0eed4e1cdb8f7144337ad0bbe796348fe7e95d98
Roberto <r.attanasio90@gmail.com>
2021-06-23 12:02:09 +0200

    commit piccole correzioni

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,8 +10,8 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
-mix.js('resources/js/app.js', 'public/js')
+//  .js('resources/js/app.js', 'public/js')
+mix
     .sass('resources/sass/styles.scss', 'public/css')
     .options({
         processCssUrls: false,
