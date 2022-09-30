44c01220cc40d78904df9961ecc10cbae7d2b46c
Alessio Pretelli <alessio.pretelli96@gmail.com>
2022-09-30 13:38:17 +0200

    aggiunta note README.md

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -13,17 +13,17 @@ class UserSeeder extends Seeder
     public function run()
     {
         $new_user = new User();
-        $new_user->name = 'Roberto';
-        $new_user->surname = 'Attanasio';
-        $new_user->address = 'Via Muzio Clementi';
-        $new_user->street_number = 49;
-        $new_user->city = 'Aprilia';
-        $new_user->county = 'Latina';
-        $new_user->password = 'roberto1234';
-        $new_user->email = 'roberto@gmail.com';
+        $new_user->name = 'Mario';
+        $new_user->surname = 'Rossi';
+        $new_user->address = 'Via Roma';
+        $new_user->street_number = 1;
+        $new_user->city = 'Roma';
+        $new_user->county = 'Roma';
+        $new_user->password = 'user1234';
+        $new_user->email = 'user@gmail.com';
         $new_user->profile_pic = '';
         $new_user->cv = '';
-        $new_user->phone_number = '3496298462';
+        $new_user->phone_number = '0123456789';
 
         $new_user->save();
     }
